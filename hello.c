aae614f5a719193010fec04a3c436d57207ec67b
fred <fredpret@protonmail.ch>
2024-04-24 21:43:48 -0600

    hello world

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What's your name?\n");
+    string number = get_string("What's your number?\n");
+    printf("hello @%s, %s! \n", number, name);
+    printf('You get 100%%!')
+}
