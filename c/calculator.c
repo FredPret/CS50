7b2503fe944dabb8cda7c517c435becd44adf7ca
fred <fredpret@protonmail.ch>
2024-04-25 13:51:56 -0600

    calculator.c

--- c/calculator.c
+++ c/calculator.c
@@ -1,1 +1,19 @@
-// calculator.c
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    long x = get_long("x: ");
+    long y = get_long("y: ");
+
+    float z = (float) x / (float) y;
+
+    printf("%f\n", z); // %li = long integer; %f = float
+    printf("%.20f", z); // %.20f = 20 decimals
+    printf(" <--- floating-point imprecision from a float\n");
+
+    double zz = (double) x / (double) y;
+    printf("%f\n", zz); // %li = long integer; %f = float
+    printf("%.20f", zz); // %.20f = 20 decimals
+    printf(" <--- less floating-point imprecision from a double\n");
+}
